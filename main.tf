ad41bb2982692a10fb84fed81a0d1f7a23f01bf6
juanasolano <juana.solano@vensure.com>
2026-05-16 21:17:00 -0500

    first commit

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0fc5d935ebf8bc3bc"
+  instance_type = "t3.micro"
+}
